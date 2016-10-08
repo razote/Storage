91d2de637fd8b172dda703cb7b084a2037e40c19
Francis Erik Razote <francis.razote@gmail.com>
2016-10-08 23:33:13 +0800

    Create Editor Activity Add Action Listener to FAB to open Editor Activity

--- app/src/main/java/com/example/android/storage/CatalogActivity.java
+++ app/src/main/java/com/example/android/storage/CatalogActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.storage;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.storage.data.StorageContract.InventoryEntry;
@@ -21,6 +24,15 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mDbHelper = new StorageDbHelper(this);
 
