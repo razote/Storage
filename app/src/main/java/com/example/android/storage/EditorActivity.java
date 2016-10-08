f7a01750e7d5ce6bbd3436361f9a71c4a72757ba
Francis Erik Razote <francis.razote@gmail.com>
2016-10-09 01:04:02 +0800

    Implement Spinner in Editor Activity

--- app/src/main/java/com/example/android/storage/EditorActivity.java
+++ app/src/main/java/com/example/android/storage/EditorActivity.java
@@ -2,12 +2,54 @@ package com.example.android.storage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.android.storage.data.StorageContract.InventoryEntry;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private Spinner mSellableSpinner;
+    private int mSellable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mSellableSpinner = (Spinner) findViewById(R.id.spinner_sellable);
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        ArrayAdapter sellableSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_boolean_option, android.R.layout.simple_spinner_item);
+
+        sellableSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        mSellableSpinner.setAdapter(sellableSpinnerAdapter);
+
+        mSellableSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.boolean_true))) {
+                        mSellable = InventoryEntry.SELLABLE_TRUE;
+                    } else {
+                        mSellable = InventoryEntry.SELLABLE_FALSE;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mSellable = InventoryEntry.SELLABLE_TRUE;
+            }
+        });
     }
 }
