1f60ff81c9b10c301431959e43e8194808c859cd
Francis Erik Razote <francis.razote@gmail.com>
2016-10-10 23:55:48 +0800

    Implement query method in StorageProvider

--- app/src/main/java/com/example/android/storage/data/StorageProvider.java
+++ app/src/main/java/com/example/android/storage/data/StorageProvider.java
@@ -1,9 +1,11 @@
 package com.example.android.storage.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 public class StorageProvider extends ContentProvider {
@@ -38,7 +40,27 @@ public class StorageProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                cursor = database.query(StorageContract.InventoryEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case PET_ID:
+                selection = StorageContract.InventoryEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = database.query(StorageContract.InventoryEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
