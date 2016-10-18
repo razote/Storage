8ce8a6d6d1dbc4f0ac69473a9b147af309aedf5a
Francis Erik Razote <francis.razote@gmail.com>
2016-10-18 21:52:58 +0800

    Editor Activity able to open and load data on clicked Inventory

--- app/src/main/java/com/example/android/storage/EditorActivity.java
+++ app/src/main/java/com/example/android/storage/EditorActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.storage;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +22,12 @@ import android.widget.Toast;
 
 import com.example.android.storage.data.StorageContract.InventoryEntry;
 
-public class EditorActivity extends AppCompatActivity {
+import static android.R.attr.data;
+
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int EXISTING_INVENTORY_LOADER = 0;
+    private Uri mCurrentInventoryUri;
 
     private EditText mName;
     private EditText mQuantity;
@@ -33,12 +42,13 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
 
         Intent intent = getIntent();
-        Uri currentPetUri = intent.getData();
+        mCurrentInventoryUri = intent.getData();
 
-        if (currentPetUri == null) {
+        if (mCurrentInventoryUri == null) {
             setTitle(getString(R.string.editor_activity_title_new_item));
         } else {
             setTitle(getString(R.string.editor_activity_title_edit_item));
+            getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
         }
 
         mName = (EditText) findViewById(R.id.edit_text_name);
@@ -129,5 +139,68 @@ public class EditorActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_INVETORY_NAME,
+                InventoryEntry.COLUMN_INVETORY_QUANTITY,
+                InventoryEntry.COLUMN_INVETORY_PRICE,
+                InventoryEntry.COLUMN_INVETORY_IMG_DIR,
+                InventoryEntry.COLUMN_INVETORY_SELLABLE };
+
+        return new CursorLoader(this,   // Parent activity context
+                mCurrentInventoryUri,         // Query the content URI for the current pet
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+
+        if (cursor.moveToFirst()) {
+            int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVETORY_NAME);
+            int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVETORY_QUANTITY);
+            int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVETORY_PRICE);
+            int imgDirColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVETORY_IMG_DIR);
+            int sellColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INVETORY_SELLABLE);
+
+            String name = cursor.getString(nameColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+            String imgDir = cursor.getString(imgDirColumnIndex);
+            int sell = cursor.getInt(sellColumnIndex);
+
+            mName.setText(name);
+            mQuantity.setText(Integer.toString(quantity));
+            mPrice.setText(Integer.toString(price));
+            mImageDir.setText(imgDir);
+
+            switch (sell) {
+                case InventoryEntry.SELLABLE_TRUE:
+                    mSellSpinner.setSelection(0);
+                    break;
+                case InventoryEntry.SELLABLE_FALSE:
+                    mSellSpinner.setSelection(1);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mName.setText("");
+        mQuantity.setText(Integer.toString(0));
+        mPrice.setText(Integer.toString(0));
+        mImageDir.setText("");
+        mSellSpinner.setSelection(0);
     }
 }
