d0968da813b4c53a95cb1b485ed8b5dcadaa06d8
Francis Erik Razote <francis.razote@gmail.com>
2016-10-18 22:24:39 +0800

    Close Editor Activity if there is nothing in the input.

--- app/src/main/java/com/example/android/storage/EditorActivity.java
+++ app/src/main/java/com/example/android/storage/EditorActivity.java
@@ -91,10 +91,23 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String quantityString = mQuantity.getText().toString().trim();
         String priceString = mPrice.getText().toString().trim();
         String img_dirString = mImageDir.getText().toString().trim();
-        int quantity = Integer.parseInt(quantityString);
-        int price = Integer.parseInt(priceString);
+
+        if (mCurrentInventoryUri == null &&
+                TextUtils.isEmpty(nameString) && TextUtils.isEmpty(img_dirString)) {
+            return;
+        }
 
         ContentValues values = new ContentValues();
+
+        int quantity = 0;
+        if (!TextUtils.isEmpty(quantityString)) {
+            quantity = Integer.parseInt(quantityString);
+        }
+
+        int price = 0;
+        if (!TextUtils.isEmpty(priceString)) {
+            price = Integer.parseInt(priceString);
+        }
 
         values.put(InventoryEntry.COLUMN_INVETORY_NAME, nameString);
         values.put(InventoryEntry.COLUMN_INVETORY_QUANTITY, quantity);
