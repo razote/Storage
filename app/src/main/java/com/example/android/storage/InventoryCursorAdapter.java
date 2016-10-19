40ec66d8498964539718a111437ed3853cdc38fc
Francis Erik Razote <francis.razote@gmail.com>
2016-10-20 00:35:27 +0800

    Deduct Quantity when Sell Button is Clicked

--- app/src/main/java/com/example/android/storage/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/storage/InventoryCursorAdapter.java
@@ -1,20 +1,33 @@
 package com.example.android.storage;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.storage.data.StorageContract;
 
 import java.text.NumberFormat;
 
+import static android.R.attr.id;
+import static com.example.android.storage.R.id.quantity;
+
 public class InventoryCursorAdapter extends CursorAdapter{
+
+    private final Context mContext;
+
     public InventoryCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
+        mContext = context;
     }
 
     @Override
@@ -25,15 +38,17 @@ public class InventoryCursorAdapter extends CursorAdapter{
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
-        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        TextView quantityTextView = (TextView) view.findViewById(quantity);
         TextView priceTextView = (TextView) view.findViewById(R.id.price);
 
+        int idColumnIndex = cursor.getColumnIndex(StorageContract.InventoryEntry._ID);
         int nameColumnIndex = cursor.getColumnIndex(StorageContract.InventoryEntry.COLUMN_INVETORY_NAME);
         int quantityColumnIndex = cursor.getColumnIndex(StorageContract.InventoryEntry.COLUMN_INVETORY_QUANTITY);
         int priceColumnIndex = cursor.getColumnIndex(StorageContract.InventoryEntry.COLUMN_INVETORY_PRICE);
 
+        final int inventoryId = cursor.getInt(idColumnIndex);
         String inventoryName = cursor.getString(nameColumnIndex);
-        int inventoryQuantity = cursor.getInt(quantityColumnIndex);
+        final int inventoryQuantity = cursor.getInt(quantityColumnIndex);
         int inventoryPrice = cursor.getInt(priceColumnIndex);
 
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
@@ -42,5 +57,31 @@ public class InventoryCursorAdapter extends CursorAdapter{
         nameTextView.setText(inventoryName);
         quantityTextView.setText("Quantity: " + Integer.toString(inventoryQuantity));
         priceTextView.setText(moneyString);
+
+        Button button = (Button) view.findViewById(R.id.click);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int inventoryOnClick = inventoryQuantity;
+
+                if(inventoryOnClick > 0){
+                    inventoryOnClick--;
+                }
+                Uri currentInventoryUri = ContentUris.withAppendedId(StorageContract.InventoryEntry.CONTENT_URI, inventoryId);
+
+                ContentValues values = new ContentValues();
+                values.put(StorageContract.InventoryEntry.COLUMN_INVETORY_QUANTITY, inventoryOnClick);
+
+                int rowsAffected = mContext.getContentResolver().update(currentInventoryUri, values, null, null);
+
+                if (rowsAffected == 0) {
+                    Toast.makeText(mContext, R.string.editor_update_inventory_failed,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, R.string.editor_update_inventory_successful,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
