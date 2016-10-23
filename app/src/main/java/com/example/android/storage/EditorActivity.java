118f973e3d0f2baba533ae9cc1fd783d06155389
Francis Erik Razote <francis.razote@gmail.com>
2016-10-23 18:08:22 +0800

    Save a valid location of the Image

--- app/src/main/java/com/example/android/storage/EditorActivity.java
+++ app/src/main/java/com/example/android/storage/EditorActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.storage;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
@@ -8,24 +9,44 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
+import android.os.ParcelFileDescriptor;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.storage.data.StorageContract.InventoryEntry;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+import static com.example.android.storage.data.StorageProvider.LOG_TAG;
+
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int PICK_IMAGE_REQUEST = 0;
+    private Uri mUri;
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mImageBtn;
 
     private static final int EXISTING_INVENTORY_LOADER = 0;
     private Uri mCurrentInventoryUri;
@@ -35,7 +56,6 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
     private EditText mName;
     private EditText mQuantity;
     private EditText mPrice;
-    private EditText mImageDir;
     private Spinner mSellSpinner;
     private int mSellVal;
 
@@ -63,19 +83,94 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
             getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
         }
 
+        mTextView = (TextView) findViewById(R.id.image_uri);
+        mImageView = (ImageView) findViewById(R.id.image);
+        mImageBtn = (Button) findViewById(R.id.get_img);
+
         mName = (EditText) findViewById(R.id.edit_text_name);
         mQuantity = (EditText) findViewById(R.id.edit_text_quantity);
         mPrice = (EditText) findViewById(R.id.edit_text_price);
-        mImageDir = (EditText) findViewById(R.id.edit_text_img_dir);
         mSellSpinner = (Spinner) findViewById(R.id.spinner_sellable);
 
         mName.setOnTouchListener(mTouchListener);
         mQuantity.setOnTouchListener(mTouchListener);
         mPrice.setOnTouchListener(mTouchListener);
-        mImageDir.setOnTouchListener(mTouchListener);
         mSellSpinner.setOnTouchListener(mTouchListener);
+        mImageBtn.setOnTouchListener(mTouchListener);
 
         setupSpinner();
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageSelector();
+            }
+        });
+    }
+
+    private void openImageSelector(){
+        Intent intent;
+        Log.e(LOG_TAG, "While is set and the ifs are worked through.");
+
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+
+        // Show only images, no videos or anything else
+        Log.e(LOG_TAG, "Check write to external permissions");
+
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        Log.i(LOG_TAG, "Received an \"Activity Result\"");
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code READ_REQUEST_CODE.
+        // If the request code seen here doesn't match, it's the response to some other intent,
+        // and the below code shouldn't run at all.
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.  Pull that uri using "resultData.getData()"
+
+            if (resultData != null) {
+                mUri = resultData.getData();
+                Log.i(LOG_TAG, "Uri: " + mUri.toString());
+
+                mTextView.setText(mUri.toString());
+                mBitmap = getBitmapFromUri(mUri);
+                mImageView.setImageBitmap(mBitmap);
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) {
+        ParcelFileDescriptor parcelFileDescriptor = null;
+        try {
+            parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(uri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            parcelFileDescriptor.close();
+            return image;
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Failed to load image.", e);
+            return null;
+        } finally {
+            try {
+                if (parcelFileDescriptor != null) {
+                    parcelFileDescriptor.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(LOG_TAG, "Error closing ParcelFile Descriptor");
+            }
+        }
     }
 
     private void setupSpinner() {
@@ -110,7 +205,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String nameString = mName.getText().toString().trim();
         String quantityString = mQuantity.getText().toString().trim();
         String priceString = mPrice.getText().toString().trim();
-        String img_dirString = mImageDir.getText().toString().trim();
+        String img_dirString = mTextView.getText().toString().trim();
 
         if (mCurrentInventoryUri == null &&
                 TextUtils.isEmpty(nameString) && TextUtils.isEmpty(img_dirString)) {
@@ -266,7 +361,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
             mName.setText(name);
             mQuantity.setText(Integer.toString(quantity));
             mPrice.setText(Integer.toString(price));
-            mImageDir.setText(imgDir);
+            mTextView.setText(imgDir);
 
             switch (sell) {
                 case InventoryEntry.SELLABLE_TRUE:
@@ -284,7 +379,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         mName.setText("");
         mQuantity.setText(Integer.toString(0));
         mPrice.setText(Integer.toString(0));
-        mImageDir.setText("");
+        mTextView.setText("");
         mSellSpinner.setSelection(0);
     }
 
